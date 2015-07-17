7e3ee1a70c74facd5cb7003d8dcc3941ea410b4b
Michael Ginn <milesper@Michaels-MacBook-Pro.local>
2015-07-17 15:06:57 -0500

    Version 1.0

--- QTRomanNumeralsExample/ViewController.swift
+++ QTRomanNumeralsExample/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  QTRomanNumeralsExample
+//  QTRomanNumeralExample
 //
 //  Created by Michael Ginn on 7/17/15.
 //  Copyright (c) 2015 Michael Ginn. All rights reserved.
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func convert(sender: AnyObject) {
+        let string = QTRomanNumerals.convertToRomanNum(numberTextField.text.toInt()!)
+        resultLabel.text = string
+    }
 
 }
 
